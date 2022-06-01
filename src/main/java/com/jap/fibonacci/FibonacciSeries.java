d499a4f255e86221e3a540c5999b14b2a765c80c
Karen Jebakany <Karen.Jebakany@in.niit.com>
2022-06-01 09:53:24 +0530

    Boilerplate added

---
+++ src/main/java/com/jap/fibonacci/FibonacciSeries.java
@@ -0,0 +1,20 @@
+package com.jap.fibonacci;
+
+public class FibonacciSeries
+{
+    // Write the logic in the method to generate the next number in the Fibonacci series
+    public long fibonacci(long number) {
+        return -1;
+    }
+    // Print the series inside the method
+    public void printFibonacciSeries(long number){
+
+    }
+    public static void main( String[] args )
+    {
+        FibonacciSeries fib = new FibonacciSeries();
+        // Call the  printFibonacciSeries() method and pass the value.
+
+
+    }
+}
---
+++ src/test/java/com/jap/fibonacci/FibonacciSeriesTest.java
@@ -0,0 +1,50 @@
+package com.jap.fibonacci;
+
+import com.jap.fibonacci.FibonacciSeries;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class FibonacciSeriesTest
+{
+
+    FibonacciSeries fib = null;
+    @Before
+   public void setUp(){
+       fib = new FibonacciSeries();
+
+   }
+    @After
+   public void tearDown(){
+      fib = null;
+
+   }
+    @Test
+    public void givenAnIntegerReturnLastNumberOfFibonacciSeries()
+    {
+        assertEquals( 55,fib.fibonacci(9));
+    }
+    @Test
+    public void givenOneReturnOne()
+    {
+        assertEquals( 1,fib.fibonacci(1));
+    }
+    @Test
+    public void givenZeroReturnOne()
+    {
+        assertEquals( 1,fib.fibonacci(0));
+    }
+    @Test
+    public void givenANegativeIntegerReturnOne()
+    {
+
+        assertEquals( 1,fib.fibonacci(-8));
+        assertNotEquals(55,fib.fibonacci(-9));
+    }
+
+}
