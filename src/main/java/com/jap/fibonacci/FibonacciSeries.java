3868cb6a17eb58731e5f0cc4c6d0b3372bf0ded4
Philip Meshach <Philipmeshach.pm@gmail.com>
2022-08-19 12:49:18 +0530

    Fibonacci series completed

--- src/main/java/com/jap/fibonacci/FibonacciSeries.java
+++ src/main/java/com/jap/fibonacci/FibonacciSeries.java
@@ -4,16 +4,31 @@ public class FibonacciSeries
 {
     // Write the logic in the method to generate the next number in the Fibonacci series
     public long fibonacci(long number) {
-        return -2;
+        if(number == 0){
+            return 0;
+        }
+        if (number == 1 || number == 2) {
+            return 1;
+        }
+        if( number < -1){
+            return -1;
+        }
+        else{
+            return fibonacci(number - 1) + fibonacci(number - 2);
+        }
     }
-    // Print the series inside the method
     public void printFibonacciSeries(long number){
-
+        for (int i = 0; i < number; i++) {
+            System.out.print(fibonacci(i)+" ");
+        }
     }
     public static void main( String[] args )
     {
         FibonacciSeries fib = new FibonacciSeries();
+        int number = 8;
         // Call the  printFibonacciSeries() method and pass the value.
+        fib.printFibonacciSeries(number);
+
 
 
     }
